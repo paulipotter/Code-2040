973691e1b27cae316fa2f8df1e84efe5a7b58928
paulipotter <paulipotter@ksu.edu>
2016-09-26 12:22:43 -0500

    added comments for step 4

--- step-4.py
+++ step-4.py
@@ -7,26 +7,34 @@ import requests, ast
 
 address = "http://challenge.code2040.org/api/prefix"
 token = "ae7949f1fc1a625459e2aed8ee66b992"
+
+# Post request should return a dictionary
 r = requests.post(address, data = {"token" : token})
+
+# The following line makes sure the variable is of type Dictionary 
 d = ast.literal_eval(r.text)
 
-prefix = d.get("prefix")
+
+prefix = d.get("prefix") 
 array = d.get("array")
-send = {"token": token, "array":[]}
+
+# Creates the dictionary that will be sent in the second POST request
+send = {"token": token, "array":[]} 
+
+
+for word in array:
+	# If word in array does not start with prefix
+	if not word.startswith(prefix):
+		send["array"].append(word)  #  append word to a new array (keep order)
+		
 
 print(prefix)
-for word in array:
-	if not word.startswith(prefix):
-		send["array"].append(word)
-		print(word)
-	else:
-		print(word, prefix)
-print(" ")
-		
-for word in send["array"]:
-	print (word)
+print()
+print(array)
+print()
+print(send)
 
-print(send["array"])
+# POST request sending a Dictionary with the resulting array and my unique API token
 address = "http://challenge.code2040.org/api/prefix/validate"
-r = requests.post(address, data= send)
+r = requests.post(address, data=send)
 print(r.text)
